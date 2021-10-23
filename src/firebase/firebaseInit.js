b3d8d484a0dd5135aa54334926b0df35bfeb11b8
Taiseer <77081440+TaiseerAlmedani@users.noreply.github.com>
2021-10-23 17:43:01 +0300

    setup firebase & link it with our vue

---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import "firebase/firestor";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAmbg3hJtg9zQVYSj3OTHkbE4NUeretYY4",
+    authDomain: "vue-blog-2f469.firebaseapp.com",
+    projectId: "vue-blog-2f469",
+    storageBucket: "vue-blog-2f469.appspot.com",
+    messagingSenderId: "1024172299566",
+    appId: "1:1024172299566:web:1800e4f680bca04f7d96bf"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+  export {timestamp};
+  export default firebaseApp.firestore();
